7a438fcec9133264efdb1ecdb0696bdc3e2815be
Daniel Marinado <daniel.marinado@gmail.com>
2022-01-11 18:11:10 -0300

    Usuarios Crud Listo -11-01-2022

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,37 +1,66 @@
 
 const { response, request } = require('express');
+const bcryptjs = require('bcryptjs');
 
-const usuariosGet = (req = request, res = response) => {
+const Usuario = require('../models/usuario');
 
-    const {q, nombre, apikey, page = 1, limit } = req.query;
+const usuariosGet = async (req = request, res = response) => {
+
+    const { limite = 5, desde = 0 } = req.query;
+
+    // const usuarios = await Usuario.find({ estado: true })
+    //     .skip( Number(desde) )
+    //     .limit( Number(limite) );
+
+    // const total = await Usuario.countDocuments({ estado: true });
+
+    const [ total, usuarios ] = await Promise.all([
+        Usuario.countDocuments({ estado: true }),
+        Usuario.find({ estado: true })
+            .skip( Number(desde) )
+            .limit( Number(limite) )
+    ]);
+
 
     res.json({
-        msg: 'get API - controlador',
-        q,
-        nombre,
-        apikey,
-        page,
-        limit
+        total, 
+        usuarios
     });
 }
 
-const usuariosPost = (req = request, res = response) => {
+const usuariosPost = async (req = request, res = response) => {
 
-    const {nombre, edad} = req.body;
+    const {nombre, correo, password, rol} = req.body;
+    const usuario = new Usuario( { nombre, correo, password, rol } );
 
-    res.status(201).json({
-        msg: 'post API - controlador', nombre, edad
+    // Encriptar la contraseña
+    const salt = bcryptjs.genSaltSync();
+    usuario.password = bcryptjs.hashSync( password, salt );
+
+    // Guardar en BD
+    await usuario.save();
+
+    res.json({
+        usuario
+
     });
 }
 
-const usuariosPut = (req = request, res = response) => {
+const usuariosPut = async (req = request, res = response) => {
 
     const id = req.params.id;
+    const { _id, password, google, correo,  ...resto } = req.body;
 
-    res.status(500).json({
-        msg: 'put API - controlador',
-        id
-    });
+    // TODO validar contra base de datos
+    if( password ) {
+        // Encriptar la contraseña
+        const salt = bcryptjs.genSaltSync();
+        resto.password = bcryptjs.hashSync( password, salt );
+    }
+
+    const usuario = await Usuario.findByIdAndUpdate( id, resto );
+
+    res.status(500).json( usuario );
 }
 
 const usuariosPatch = (req = request, res = response) => {
@@ -40,10 +69,16 @@ const usuariosPatch = (req = request, res = response) => {
     });
 }
 
-const usuariosDelete = (req = request, res = response) => {
-    res.json({
-        msg: 'delete API - controlador'
-    });
+const usuariosDelete = async (req = request, res = response) => {
+
+    const { id } = req.params;
+
+    // Fisicamente lo borramos
+    // const usuario = await Usuario.findByIdAndDelete( id );
+
+    const usuario = await Usuario.findByIdAndUpdate( id, { estado: false } );
+
+    res.json( usuario );
 }
 
 module.exports = {
---
+++ models/usuario.js
@@ -0,0 +1,44 @@
+
+const { Schema, model } = require('mongoose');
+
+const UsuarioSchema = Schema({
+    nombre: {
+        type: String,
+        required: [true, 'El nombre es obligatorio']
+    },
+    correo: {
+        type: String,
+        required: [true, 'El correo es obligatorio'],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, 'La contraseña es obligatoria']
+    },
+    img: {
+        type: String,
+    },
+    rol: {
+        type: String,
+        required: true,
+        enum: ['ADMIN_ROLE', 'USER_ROLE']  //pusieron EMUN
+    },
+    estado: {
+        type: Boolean,
+        default: true
+    },
+    google: {
+        type: Boolean,
+        default: false
+    }
+
+});
+
+// Método para ocultar la versión y el password
+// Se esta usando en el post cuando se imprime el schema
+UsuarioSchema.methods.toJSON = function() {
+    const { __v, password, ...usuario } =  this.toObject();
+    return usuario;
+}
+
+module.exports = model( 'Usuario', UsuarioSchema )
